cf1554c957345f8a92ecc9a8cec4a93516b2e400
Nazik-Bolotbekova <bolotbekova09@gmail.com>
2026-04-26 05:18:37 +0600

    s3 conf

--- core/settings.py
+++ core/settings.py
@@ -86,7 +86,7 @@ STORAGES = {
         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
